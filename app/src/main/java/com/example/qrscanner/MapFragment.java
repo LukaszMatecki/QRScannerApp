987dae06cc1bf53b4ce43c6406e693babbf1c3dc
Łukasz Matecki <UL0275834@edu.uni.lodz.pl>
2024-12-07 19:49:01 +0100

    Working Google Maps API & added icons custom made in Adobe Illustrator

--- app/src/main/java/com/example/qrscanner/MapFragment.java
+++ app/src/main/java/com/example/qrscanner/MapFragment.java
@@ -1,18 +1,33 @@
 package com.example.qrscanner;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 public class MapFragment extends Fragment {
 
+    Button showmap;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Zwracamy widok fragmentu, a nie wynik z inflater.inflate
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        // Zwracamy widok fragmentu
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        // Inicjalizujemy przycisk
+        showmap = view.findViewById(R.id.showmap);
+
+        // Ustawiamy listener na kliknięcie
+        showmap.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), MapsActivity.class);
+            startActivity(intent);
+        });
+
+        return view;
     }
 }
